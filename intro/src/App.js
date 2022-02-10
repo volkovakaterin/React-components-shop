2fb6232def63672b1236da652015d1071fb5238c
Ekaterina Volkova <volkovakaterin@gmail.com>
2022-02-10 21:15:19 +0400

    changes

--- intro/src/App.js
+++ intro/src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 import ShopItemFunc from './components/ShopItemFunc';
 
 function App() {
-    return <ShopItemFunc/>;
+    const item = {
+        brand: 'Tiger of Sweden',
+        title: 'Leonard coat',
+        description: 'Minimalistic coat in cotton-blend',
+        descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+        price: 399,
+        currency: '£'
+      }
+    return <ShopItemFunc item = {item}/>;
 
 }
 
--- intro/src/components/ShopItemFunc.js
+++ intro/src/components/ShopItemFunc.js
@@ -1,14 +1,7 @@
 import React from 'react';
 
-function ShopItemFunc() {
-    const item = {
-        brand: 'Tiger of Sweden',
-        title: 'Leonard coat',
-        description: 'Minimalistic coat in cotton-blend',
-        descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-        price: 399,
-        currency: '£'
-      }
+function ShopItemFunc(props) {
+    const{item}=props;
       return (
           <div className='main-content'>
             <h2>{item.brand}</h2>
